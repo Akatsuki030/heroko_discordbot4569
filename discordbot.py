430fb215e903d0ddd09185af1dd19f98f2ec285c
GFVictorique <97880270+GFVictorique@users.noreply.github.com>
2022-01-20 13:55:14 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -24,15 +24,15 @@ async def on_message(message):
     if client.user in message.mentions: # @判定
         translator = googletrans.Translator()
         robotName = client.user.name
-        first, space, content = message.clean_content.partition('tw @'+robotName+'') 
-
-      
+        first, space, content = message.clean_content.partition('@'+robotName+' ')
         
-
+        if content == '':
+            content = first
+        if translator.detect(content).lang == DSTLanguage:
+            return
         if translator.detect(content).lang == SRCLanguage or SRCLanguage == '':
-            remessage = translator.translate(content, dest= 'zh_TW').text
+            remessage = translator.translate(content, dest= DSTLanguage).text
             await message.reply(remessage) 
 
-   
 # Bot起動
 client.run(TOKEN)
