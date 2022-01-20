9a11fa5140c43574cdb3360c169ff2a68a0e704d
GFVictorique <97880270+GFVictorique@users.noreply.github.com>
2022-01-20 13:00:27 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,3 +1,23 @@
+import discord
+import googletrans
+import os
+from pprint import pprint
+# 輸入自己Bot的TOKEN碼
+TOKEN = os.environ['TOKEN']
+SRCLanguage=os.environ['SRC']
+DSTLanguage=os.environ['DST']
+
+client = discord.Client()
+
+# 起動時呼叫
+@client.event
+async def on_ready():
+    print('成功登入')
+
+# 收到訊息時呼叫
+@client.event
+async def on_message(message):
+    # 送信者為Bot時無視
     if message.author.bot:
         return
     
@@ -10,12 +30,37 @@
         if content == '':
             content = first
         
-      
+        if translator.detect(content).lang == DSTLanguage:
+        return
 
         if translator.detect(content).lang == SRCLanguage or SRCLanguage == '':
             remessage = translator.translate(content, dest= 'zh_TW').text
             await message.reply(remessage) 
 
-    
+    if client.user in message.mentions: # @判定
+        translator = googletrans.Translator()
+        robotName = client.user.name
+        first, space, language, content = message.clean_content.partition('@'+robotName+'en: '+'') 
+        
+        if content == '':
+            content = first
+        if translator.detect(content).lang == DSTLanguage:
+            return
+        if translator.detect(content).lang == SRCLanguage or SRCLanguage == '':
+            remessage = translator.translate(content, dest= 'en_US').text
+            await message.reply(remessage) 
+            
+    if client.user in message.mentions: # @判定
+        translator = googletrans.Translator()
+        robotName = client.user.name
+        first, space, language, content = message.clean_content.partition('@'+robotName+'ja: '+'') 
+        
+        if content == '':
+            content = first
+        if translator.detect(content).lang == DSTLanguage:
+            return
+        if translator.detect(content).lang == SRCLanguage or SRCLanguage == '':
+            remessage = translator.translate(content, dest= 'ja').text
+            await message.reply(remessage) 
 # Bot起動
 client.run(TOKEN)
